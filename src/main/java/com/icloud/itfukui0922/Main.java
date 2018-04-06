c94f4afcb4200e5f68998e890592eea883afb05e
梟 <>
2018-04-06 18:31:47 +0900

    import追加

--- src/main/java/com/icloud/itfukui0922/Main.java
+++ src/main/java/com/icloud/itfukui0922/Main.java
@@ -1,5 +1,8 @@
 package com.icloud.itfukui0922;
 
+import com.icloud.itfukui0922.player.AITWolf;
+import com.icloud.itfukui0922.player.Dammy;
+import com.icloud.itfukui0922.starter.LocalHostStarter;
 import org.aiwolf.common.data.Player;
 
 import java.util.ArrayList;
